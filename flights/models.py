7e199834664072f745c87c62dc47483c357ff81e
Veeresh-Hanni <veereshhanni347@gmail.com>
2025-07-05 21:50:43 +0530

    Add is_valid_flight method to Flight model

--- flights/models.py
+++ flights/models.py
@@ -14,15 +14,18 @@ class  Airport(models.Model):
 
 
 class Flight(models.Model):
-    origin = models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departures")
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration > 0
+
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
     
-    def is_valid_flight(self):
-        return self.origin != self.destination  and self.duration > 0
+    
 
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
